02f08822e4dfc14a9f06d37fdbb7af27d7c44d03
Joshua <mramos29@ucol.mx>
2021-08-22 17:09:32 -0500

    Sucesión de 1 a 999

--- main.js
+++ main.js
@@ -1,2 +1,18 @@
-import Coladera from "./coladera.js";
+class App{
+    constructor(){
+        this.cero = new Array();
+        this.uno = new Array();
+        for(let i = 1; i < 1000; i++){
+            if(i<1000){
+                this.uno.push(i)
+            }
+        }
+        
+        console.log(this.uno);
+    }
 
+    primosColadera(){
+        
+    }
+}
+new App;
