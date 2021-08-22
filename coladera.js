09b002a4ff3e0aa9830d6735a3192e9592f538f5
Joshua <mramos29@ucol.mx>
2021-08-22 16:47:02 -0500

    AHORA ENTIENDO EL PROCESO

--- coladera.js
+++ coladera.js
@@ -1,3 +1,12 @@
 export default class Coladera{
-    
+    constructor(){
+        let cero = new Array();
+        let uno = new Array();
+    }
+
+    primosColadera(){
+        for(let i = 0; i < 1000; i++){
+
+        }
+    }
 }
