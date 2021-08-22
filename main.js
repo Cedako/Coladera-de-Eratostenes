3efc1956c34736b29856eb10cc916877f7aaf85d
Joshua <mramos29@ucol.mx>
2021-08-22 17:54:37 -0500

    first try, in progress

--- main.js
+++ main.js
@@ -7,12 +7,17 @@ class App{
                 this.uno.push(i)
             }
         }
-        
-        console.log(this.uno);
-    }
-
-    primosColadera(){
-        
+        //Comprobación de que el vector con todos los números del 1 al 999 funciona
+        for(let i = 1; i < 1000; i++){
+            if(this.uno[i]){
+                for(let j = 1 + this.uno[i];j < 1000; j++){
+                    this.cero.push(this.uno[j]);
+                    this.uno[j] = false;
+                }
+            }
+        }
+        console.log(this.cero);
+        console.log(this.uno[1]);
     }
 }
 new App;
