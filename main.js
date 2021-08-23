497b29624f46e04afece99d57a475964ee6e1ca6
Joshua <mramos29@ucol.mx>
2021-08-23 18:54:03 -0500

    okay, hay problemas

--- main.js
+++ main.js
@@ -3,13 +3,13 @@ class App{
     constructor(){
         this.cero = new Array();
         this.uno = new Array();
-        for(let i = 1; i < 100; i++){
+        for(let i = 1; i < 1000; i++){
             if(i){
                 this.uno.push(i)
             }
         }
         this.eratostenes2ndTry();
-        console.log(this.uno);
+        //console.log(this.uno);
         console.log(this.cero);
 
         //Eliminiación de "false" del array.
@@ -45,6 +45,54 @@ class App{
             else if ((this.uno[i]%7 === 0) && (this.uno[i]>7)){
                 this.cero.push(this.uno[i])
                 for(let j=i;j<this.uno.length;j+=7){
+                    this.uno[i]=false;
+                }
+            }
+            else if ((this.uno[i]%11 === 0) && (this.uno[i]>11)){
+                this.cero.push(this.uno[i])
+                for(let j=i;j<this.uno.length;j+=11){
+                    this.uno[i]=false;
+                }
+            }
+            else if ((this.uno[i]%13 === 0) && (this.uno[i]>13)){
+                this.cero.push(this.uno[i])
+                for(let j=i;j<this.uno.length;j+=13){
+                    this.uno[i]=false;
+                }
+            }
+            else if ((this.uno[i]%17 === 0) && (this.uno[i]>17)){
+                this.cero.push(this.uno[i])
+                for(let j=i;j<this.uno.length;j+=17){
+                    this.uno[i]=false;
+                }
+            }
+            else if ((this.uno[i]%19 === 0) && (this.uno[i]>19)){
+                this.cero.push(this.uno[i])
+                for(let j=i;j<this.uno.length;j+=19){
+                    this.uno[i]=false;
+                }
+            }
+            else if ((this.uno[i]%23 === 0) && (this.uno[i]>23)){
+                this.cero.push(this.uno[i])
+                for(let j=i;j<this.uno.length;j+=23){
+                    this.uno[i]=false;
+                }
+            }
+            else if ((this.uno[i]%29 === 0) && (this.uno[i]>29)){
+                this.cero.push(this.uno[i])
+                for(let j=i;j<this.uno.length;j+=29){
+                    this.uno[i]=false;
+                }
+            }
+            else if ((this.uno[i]%31 === 0) && (this.uno[i]>31)){
+                this.cero.push(this.uno[i])
+                for(let j=i;j<this.uno.length;j+=31){
+                    this.uno[i]=false;
+                }
+            }
+            else if ((this.uno[i]%53 === 0) && (this.uno[i]>53)){
+                this.cero.push(this.uno[i])
+                for(let j=i;j<this.uno.length;j+=53){
                     this.uno[i]=false;
                 }
             }
