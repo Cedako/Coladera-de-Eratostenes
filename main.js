204e3535bd07ecde955d0a56d192c3cee4728a45
Joshua <mramos29@ucol.mx>
2021-08-23 18:08:42 -0500

    Parece funcionar, toca buscar los multiplos de 7

--- main.js
+++ main.js
@@ -1,23 +1,42 @@
+
 class App{
     constructor(){
         this.cero = new Array();
         this.uno = new Array();
-        for(let i = 1; i < 1000; i++){
-            if(i<1000){
+        for(let i = 1; i < 20; i++){
+            if(i){
                 this.uno.push(i)
             }
         }
+        this.eratostenes2ndTry();
+        console.log(this.uno);
+        console.log(this.cero);
         //Comprobación de que el vector con todos los números del 1 al 999 funciona
-        for(let i = 1; i < 1000; i++){
-            if(this.uno[i]){
-                for(let j = 1 + this.uno[i];j < 1000; j++){
-                    this.cero.push(this.uno[j]);
-                    this.uno[j] = false;
+        
+    }
+
+    eratostenes2ndTry(){
+        for(let i = 0; i<this.uno.length; i++){
+            if ((this.uno[i]%2 === 0) && (this.uno[i]>2)){
+                this.cero.push(this.uno[i])
+                for(let j=i;j<this.uno.length;j+=2){
+                    this.uno[i]=false;
+                }
+            }
+            else if ((this.uno[i]%3 === 0) && (this.uno[i]>3)){
+                this.cero.push(this.uno[i])
+                for(let j=i;j<this.uno.length;j+=3){
+                    this.uno[i]=false;
+                }
+            }
+            else if ((this.uno[i]%5 === 0) && (this.uno[i]>5)){
+                this.cero.push(this.uno[i])
+                for(let j=i;j<this.uno.length;j+=5){
+                    this.uno[i]=false;
                 }
             }
         }
-        console.log(this.cero);
-        console.log(this.uno[1]);
     }
+
 }
 new App;
